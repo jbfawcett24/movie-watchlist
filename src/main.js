941dc9f718e53dc6eaa7e1642a37f1f1a35013d5
jbfawcett24 <jbfawcett24@gmail.com>
2025-03-18 10:21:42 -0600

    basic selecting of playlists

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const auth = import.meta.env.VITE_TMDB_AUTH_Key;
-const url = 'https://api.themoviedb.org/3/movie/597';
+const url = 'https://api.themoviedb.org/3/movie/';
+const imageBaseUrl = "https://image.tmdb.org/t/p/w500";
 const options = {
     method: "GET",
     headers: {
@@ -8,7 +9,46 @@ const options = {
     }
 }
 
-fetch(url, options)
-    .then(res => res.json())
-    .then(json => console.log(json))
-    .catch(err => console.error(err));
+let playlists = [
+        {
+            "name": "playlist 1",
+            "movieId": [11, 330459]
+        }
+    ];
+function init()
+{
+    playlists = JSON.parse(localStorage.getItem("playlists"));
+    setPage();
+}
+function sideTemplate(name)
+{
+    return `<li>
+        <span id="${name.replaceAll(" ", "-")}">${name}</span>
+   </li>`
+}
+function movieTemplate(movie)
+{
+    return `<a href="movie.html" class="movieContainer" id="${movie.title.replaceAll(" ", "-")}">
+        <img src="${imageBaseUrl + movie.poster_path}" alt="${movie.title} Poster">
+            <h3>${movie.title}</h3>
+    </a>`
+}
+async function setMovies(e)
+{
+    const movieSection = document.querySelector("#movies");
+    const id = e.target.id;
+    const selectedPlaylist = playlists.find(playlist => playlist.name === id.replaceAll("-", " "));
+    const moviePromises = selectedPlaylist.movieId.map(movie => fetch(url + movie, options).then(res => res.json()));
+    const movies = await Promise.all(moviePromises);
+    const html = movies.map(movieTemplate);
+    movieSection.innerHTML = html.join("");
+}
+function setPage(){
+    const sideMenu = document.querySelector('#sideMenu > ul');
+    playlists.forEach(playlist => {
+        sideMenu.insertAdjacentHTML('afterbegin', sideTemplate(playlist.name));
+        document.querySelector(`#${playlist.name.replaceAll(" ", "-")}`).addEventListener("click", setMovies);
+    });
+}
+
+init();
