ffd224eac1f0d5a5531ac83fc16485ab889cea88
jbfawcett24 <jbfawcett24@gmail.com>
2025-04-02 10:58:31 -0600

    most js done

--- src/js/share.mjs
+++ src/js/share.mjs
@@ -6,9 +6,12 @@ if(localStorage.getItem("playlists")) {
 const params = new URLSearchParams(window.location.search);
 let idString = params.get("movieID");
 console.log(idString);
-playlists.push({
-    "name": `${params.get("name")}`,
-    "movieId": idString.split(",")
-});
-localStorage.setItem("playlists", JSON.stringify(playlists));
+if(!playlists.some(playlist => playlist.name === params.get("name")))
+{
+    playlists.push({
+        "name": `${params.get("name")}`,
+        "movieId": idString.split(",")
+    });
+    localStorage.setItem("playlists", JSON.stringify(playlists));
+}
 window.location.href = "index.html";
